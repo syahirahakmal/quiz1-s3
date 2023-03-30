1b13b2e8ffb10ff40f6a3be58c8873101bce6d26
Syahirah <wsaakmal@gmail.com>
2023-03-30 15:52:55 +0800

    messages

---
+++ quiz1.js
@@ -0,0 +1,43 @@
+let message = "benr"
+console.log(message, typeof message)
+
+
+let str = "benr"
+let num = 123
+
+let total= parseInt(str) + num
+console.log(total, typeof total)
+
+let data = ["benr", 1234, true]
+
+console.log(data[0])
+console.log(data[1])
+console.log(data[2])
+console.log(data[3])
+
+let user =
+{
+    name:  "syahirah",
+    age: 22, 
+    faculty: "FKEKK",
+
+}
+
+console.log(user.name)
+console.log(user.faculty)
+
+let messages = [
+
+{   name: "user1", 
+    date:"1/4/23", 
+    msg:"goood video",
+    like: 1000
+}, 
+{   name: "user2", 
+    date:"1/5/23", 
+    msg:"bad video",
+    like: 1000
+}
+]
+
+console.log(messages[0].msg) //  good video 
